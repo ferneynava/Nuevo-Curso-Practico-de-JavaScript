4fad8ead051b3c69d26083db166bc3d7bd8133cf
ferneynava <ferneynava@gmail.com>
2022-08-13 15:55:24 -0500

    :white_check_mark: Add: continuación curso de addEventListener
    Add: continuación curso de addEventListener

--- addEventListener/script.js
+++ addEventListener/script.js
@@ -18,6 +18,8 @@ const pResultSumaMath = document.querySelector("#resultSumaMath");
 //     pResultSumaMath.innerText = "Resultado con Operación Matemática:" + sumaOperación; 
 // }
 
+//// Forma 1 formulario submit 
+
 // form.addEventListener("submit", sumarInputValues); // submit se activa cuando el formulario es enviado, normalmente se utiliza para validar el formulario antes de ser enviado
 //                                                     //al servidor. 
 
@@ -29,3 +31,39 @@ const pResultSumaMath = document.querySelector("#resultSumaMath");
 //     pResultString.innerText = "Resultado con concatenación: " + sumaString;
 //     pResultSumaMath.innerText = "Resultado con Operación Matemática:" + sumaOperación; 
 // }
+
+//// Forma 2 button 
+
+//btn.addEventListener("click", sumarInputValues); // submit se activa cuando el formulario es enviado, normalmente se utiliza para validar el formulario antes de ser enviado
+                                                     //al servidor. 
+
+//function sumarInputValues(event){ //funcion que se ejecuta cuando le damos click al boton.
+    //console.log({event});  
+    //event.preventDefault();
+  //  const sumaString = input1.value + input2.value; //Los input guarda todo lo que escriban los usarios en String, por lo tanto esta linea de codigo concatena los dos input 
+   // const sumaOperación = Number(input1.value) + Number(input2.value); //Number ==> convierte los string a numeros de esta forma podemos realizar operaciones matemáticas. 
+    //pResultString.innerText = "Resultado con concatenación: " + sumaString;
+    //pResultSumaMath.innerText = "Resultado con Operación Matemática:" + sumaOperación; 
+//}
+
+/// evento keydown ==> evento se dispara cuando se presiona una tecla
+
+// addEventListener("keydown", sumarInputValues);
+
+// function sumarInputValues(event){ //funcion que se ejecuta cuando le damos click al boton.
+//     if(event.key == "Enter"){
+//         const sumaString = input1.value + input2.value; //Los input guarda todo lo que escriban los usarios en String, por lo tanto esta linea de codigo concatena los dos input 
+//         const sumaOperación = Number(input1.value) + Number(input2.value); //Number ==> convierte los string a numeros de esta forma podemos realizar operaciones matemáticas. 
+//         pResultString.innerText = "Resultado con concatenación: " + sumaString;
+//         pResultSumaMath.innerText = "Resultado con Operación Matemática:" + sumaOperación; 
+//     }  
+// }
+
+/// evento mouseover ==> evento se activa cuando se usa un dispositivo señalador (como un mouse o un trackpad) para mover el cursor al elemento o uno de sus elementos secundarios. 
+
+// addEventListener("mouseover", () => {
+//     const sumaString = input1.value + input2.value; //Los input guarda todo lo que escriban los usarios en String, por lo tanto esta linea de codigo concatena los dos input 
+//     const sumaOperación = Number(input1.value) + Number(input2.value); //Number ==> convierte los string a numeros de esta forma podemos realizar operaciones matemáticas. 
+//     pResultString.innerText = "Resultado con concatenación: " + sumaString;
+//     pResultSumaMath.innerText = "Resultado con Operación Matemática:" + sumaOperación; 
+// })
